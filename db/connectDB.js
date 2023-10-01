400decfc152c00fe0ce99b19543b90aba36d105a
EJ Milord <jmilord7@gmail.com>
2023-10-01 02:21:14 -0400

    debugging db connection

--- db/connectDB.js
+++ db/connectDB.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose')
 // Get access to ENV file so I can get URI for database
 require('dotenv').config()
 
+console.log(process.env.MONGODB_URI)
 async function dbConnect(){
+    
     // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
     mongoose
     .connect(process.env.MONGODB_URI)
