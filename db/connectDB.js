edc128012b097ca4f953e3d0eb14d9888e794b73
EJ Milord <jmilord7@gmail.com>
2023-10-01 01:52:11 -0400

    update MongoDB variable

--- db/connectDB.js
+++ db/connectDB.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 async function dbConnect(){
     // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
     mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGODB_URI)
         .then(() => {
             console.log("Database connection successful")
         })
